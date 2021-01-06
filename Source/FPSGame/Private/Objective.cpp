6c187c752768b4b0d933836feab693a253a466a7
Calvin Wong <ctwong958@berkeley.edu>
2021-01-06 13:46:30 -0800

    Initial objective finished

---
+++ Source/FPSGame/Private/Objective.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Objective.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AObjective::AObjective()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	RootComponent = Mesh;
+	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>("Hitbox");
+	SphereComp->SetupAttachment(Mesh);
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+// Called when the game starts or when spawned
+void AObjective::BeginPlay()
+{
+	Super::BeginPlay();
+	PlayFX();
+}
+
+// Called every frame
+//void AObjective::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
+
+void AObjective::NotifyActorBeginOverlap(AActor* OtherActor) 
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	PlayFX();
+	Destroy();
+}
+
+void AObjective::PlayFX() 
+{
+	UGameplayStatics::SpawnEmitterAtLocation(this, PickupFX, GetActorLocation());
+}
